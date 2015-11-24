05709ac64d06146dcce037d477e303c14df1f72b
Quentin Tronquoy <quentin-244@hotmail.fr>
2015-11-24 13:57:13 +0100

    remise en place du repo

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <?php
 	try {
-	$connexion = new PDO('mysql:host=localhost;dbname=test','root','');
+	$connexion = new PDO('mysql:host=localhost;dbname=library','root','');
 		}
 	
 	catch(Exception $e)
