0038d93cd20e5f6cf92092dc58856678011d8ad5
Eliott SECLY <eliott@kizym.com>
2015-11-24 13:48:40 +0100

    New git

---
+++ includes/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<?php
+	try {
+	$connexion = new PDO('mysql:host=localhost;dbname=test','root','');
+		}
+	
+	catch(Exception $e)
+	{
+		echo 'Erreur de connexion a la Base de données :' . $e->getMessage();
+		die(); 
+	}
+
+$tronquoy = "noob";
+
+ ?>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>OLibrary</title>
+	<link rel="icon" href="./images/favicon.ico" />
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900|Quicksand:400,700|Questrial" rel="stylesheet" />
+	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+</head>
+
+<body>
+<div id="header-wrapper">
+	<div id="header" class="container">
+		<div class="logo">
+        	<!-- <span class="icon icon-cog"></span> -->
+			
+			<img id="logo" src="images/logo.png">
+			<a href="./index.php" class="ol">OLibrary</a>
+
+			<?php echo "quentin est un gros ".$tronquoy; ?>
+			
+			<div class="clear"></div>
+		
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php include_once("includes/header.php");
+?>
+
+
+
+
+<div id="menu">
+			<ul>
+				<li class="active"><a href="./index.php" accesskey="1" title="Page d'Accueil">Accueil</a></li>
+				<li><a href="./emprunte.php" accesskey="2" title="Page de notre école">Livre emprunté</a></li>
+				<li><a href="./reserver.php" accesskey="3" title="Fiche d'inscription">Réserver un livre</a></li>
+				<li><a href="./connexion.php" accesskey="4" title="Connexion">Connexion</a></li>
+	
+		
+	
+</div>
+
+<img id="banner" src="images/banner.jpeg">
+
+
+<body>
+
+
+<div class="title">
+	  <h2>Bienvenue sur le site de OLibrary </h2>
+		
+		<p>  OLibrary est une base de donnée de livre. <br>
+			
+		</p>
+</div>
+
+</body>
+
+</div>
+</div>
+<?php
+include_once("includes/footer.php");
+?>
