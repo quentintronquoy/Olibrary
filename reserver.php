1f1804e135be8f08c0af6979992a888b25fc393d
Quentin Tronquoy <quentin-244@hotmail.fr>
2015-12-02 15:32:15 +0100

    affichage des livre dans emprunté

--- reserver.php
+++ reserver.php
@@ -1,20 +1,41 @@
-<?php include_once("includes/header.php");
+<?php 
+include_once("includes/header.php");
 ?>
+<div id="menu">
+			<ul>
+				<li class="active"><a href="./index.php" accesskey="1" title="Page d'Accueil">Accueil</a></li>
+				<li><a href="./connexion.php" accesskey="4" title="deconnexion">Connexion</a></li>
+			</ul>
+	
+		
+	
+</div>
+
+<img id="banner" src="images/banner.jpeg">
 
 <div>
-Regardez vos emprunts : <input type="submit" value="envoyer" name="go" />
+Regardez vos emprunts : <input type="submit" value="afficher" name="go" />
 </div>
 <?php
 
-   if(isset($_POST['go'])) 
-    {
+
+    		echo "OK";
 			$query=$connexion->prepare("SELECT livre_exemplaire, emprunt_date, emprunt_retour 
 			FROM emprunt
 			WHERE utilisateur_numdecompte = " . $_SESSION['id'] . "");
 			$query->execute();
 			var_dump($query);
-    }  
-    else 
+			while($donnees = $query->fetch()){
+			echo "numero de l'exemplaire :";
+			echo $donnees['livre_exemplaire']."</br>";
+			echo "date de l'emprunt :";
+			echo $donnees['emprunt_date']."</br>";
+			echo "date de retour :";
+			echo $donnees['emprunt_retour']."</br>";
+												}
+       if(isset($_POST['go'])) 
+    {		}  
+    else
     {
 	        echo '<p> Vous n\'avez pas encore emprunté de livre</p>
 	        <p>Cliquez <a href="./index.php">ici</a> pour revenir</p>';
